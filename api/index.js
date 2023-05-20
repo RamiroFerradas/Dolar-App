ffc2f1392e1821ffde7c5397320e3caa8607010d
RamiroFerradas <ramiferra97@gmail.com>
2023-05-20 16:39:53 -0300

    manejo de error

--- api/index.js
+++ api/index.js
@@ -1,12 +1,17 @@
 const app = require("./src/app.js");
 const { swaggerDocs } = require("./src/routes/swagger.js");
 
-// const { swaggerDocs } = require("./src/routes/swagger.js");
-
 const PORT = process.env.PORT || 7070;
+const DOLARSI_URL = process.env.DOLARSI_URL; // Asumiendo que DOLARSI_URL es una variable de entorno
 
-// Starting the server
+// Comprobación de la configuración de la URL
+if (!DOLARSI_URL) {
+  throw new Error("URL API NO CONFIGURADA");
+}
+
+// Iniciando el servidor
 app.listen(PORT, async () => {
+  console.log("✅ URL API CONFIGURADA");
   console.log(`🚀 Server listening at port: ${PORT}`);
   swaggerDocs(app, PORT);
 });
